d4987a89a6c6ee2a2e5096c50da3d88c8eee2dde
TheGuru-main <idrisdfirst@gmail.com>
2026-07-14 22:55:15 +0100

    Create backend/platform/models/__init__.py
    initupdate

---
+++ backend/platform/models/__init__.py
@@ -0,0 +1,31 @@
+"""
+LOMINII ORM Models
+Registers every SQLAlchemy model with Base.metadata.
+"""
+
+# Public
+from .public import *
+
+# Search
+from .search import *
+
+# Messaging
+from .messages import *
+
+# Social
+from .social import *
+
+# Education
+from .edu import *
+
+# Games
+from .games import *
+
+# Ads
+from .ads import *
+
+# Admin
+from .admin import *
+
+# Crawler
+from .crawler import *
