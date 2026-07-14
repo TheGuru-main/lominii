3b21608c17318dcc8a73d3f25e331f3470aed91c
TheGuru-main <idrisdfirst@gmail.com>
2026-07-14 13:16:52 +0100

    Update messages.py

--- apps/lominii/social/messages.py
+++ apps/lominii/social/messages.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
-from typing import List, Optional
+from typing import List, Optional, Dict
 from uuid import UUID
 
 from platform.database import get_db
