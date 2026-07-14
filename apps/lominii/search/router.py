d87b067261182d12c439b741b08bc5d35085924d
TheGuru-main <idrisdfirst@gmail.com>
2026-07-14 21:26:42 +0100

    Update router.py

--- apps/lominii/search/router.py
+++ apps/lominii/search/router.py
@@ -13,7 +13,7 @@ from platform.auth import get_current_user
 from platform.database import get_db
 from platform.gsg import gps_to_gsg
 from platform.intent_analyzer import analyze
-from platform.models.public import User, OTP
+from platform.models.public import User,
 from platform.models.search import (
     Search,
     Trending,
