5c59e8f4a723539e46332aee5b409242ffc12a3f
TheGuru-main <idrisdfirst@gmail.com>
2026-07-14 21:39:31 +0100

    Update public.py

--- backend/platform/models/public.py
+++ backend/platform/models/public.py
@@ -3,14 +3,25 @@ import uuid
 from sqlalchemy import (
     Column,
     Integer,
+    SmallInteger,
     String,
+    Float,
     DateTime,
+    ForeignKey,
     Boolean,
     JSON,
+    Text,
 )
 
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import (
+    UUID,
+    JSONB,
+)
+
+from sqlalchemy.orm import relationship
+
 from platform.database import Base
+from platform.nsid import NSID
 
 
 # ===========================================================================
