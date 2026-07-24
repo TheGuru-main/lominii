1decad84cccbd65aedd087e9804b8532877d30fa
TheGuru-main <idrisdfirst@gmail.com>
2026-07-24 02:02:52 +0100

    Create backend/platform/keymaps/registry.py

---
+++ backend/platform/keymaps/registry.py
@@ -0,0 +1,9 @@
+from .english import KEYBOARD as ENGLISH
+from .french import KEYBOARD as FRENCH
+from .german import KEYBOARD as GERMAN
+
+KEYMAPS = {
+    "en": ENGLISH,
+    "fr": FRENCH,
+    "de": GERMAN,
+}
