c16d40c255c7f05ad94f0e241260c61813f8b9d8
TheGuru-main <idrisdfirst@gmail.com>
2026-07-24 02:52:21 +0100

    Create backend/platform/dictionaries/english.py

---
+++ backend/platform/dictionaries/english.py
@@ -0,0 +1,16 @@
+"""
+English Dictionary
+"""
+
+from .base import DictionaryEntry
+
+LANGUAGE = "en"
+
+NAME = "English"
+
+SCRIPT = "Latin"
+
+DIRECTION = "ltr"
+
+
+WORDS: dict[str, DictionaryEntry] = {}
