367728059b7539bc3e1680a67ed33f1eace72651
TheGuru-main <idrisdfirst@gmail.com>
2026-07-24 02:55:04 +0100

    Create backend/platform/dictionaries/french.py

---
+++ backend/platform/dictionaries/french.py
@@ -0,0 +1,16 @@
+"""
+French Dictionary
+"""
+
+from .base import DictionaryEntry
+
+LANGUAGE = "fr"
+
+NAME = "French"
+
+SCRIPT = "Latin"
+
+DIRECTION = "ltr"
+
+
+WORDS: dict[str, DictionaryEntry] = {}
