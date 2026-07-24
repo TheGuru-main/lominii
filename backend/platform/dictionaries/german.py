d7bd3a3e4ef388068e38c1a0fc3820be4261296a
TheGuru-main <idrisdfirst@gmail.com>
2026-07-24 02:56:12 +0100

    Create backend/platform/dictionaries/german.py

---
+++ backend/platform/dictionaries/german.py
@@ -0,0 +1,16 @@
+"""
+German Dictionary
+"""
+
+from .base import DictionaryEntry
+
+LANGUAGE = "de"
+
+NAME = "German"
+
+SCRIPT = "Latin"
+
+DIRECTION = "ltr"
+
+
+WORDS: dict[str, DictionaryEntry] = {}
